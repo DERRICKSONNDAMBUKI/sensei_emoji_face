977ff0dd37f421fac5b1da594d6a36b635ea48c5
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2021-12-23 19:14:24 +0300

    complete smiley emoji face

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import { arc, path } from "d3";
-
+import { Face } from "./components/Face";
 
 const width = 960;
 const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 30;
-const eyeOffsetX = 90;
-const eyeOffsetY = 100;
-const eyeRadius = 40;
-const mouthWidth = 20
-const mouthRadius = 140
 
-const mouthArc = arc()
-  .innerRadius(mouthRadius)
-  .outerRadius(mouthRadius+mouthWidth)
-  .startAngle(Math.PI / 2)
-  .endAngle((Math.PI * 3) / 2);
-
-function App() {
-  return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${centerX},${centerY})`}>
-        <circle
-          r="245"
-          fill="yellow"
-          stroke="black"
-          stroke-width={strokeWidth}
-        />
-        <circle r={eyeRadius} cx={-eyeOffsetX} cy={-eyeOffsetY} />
-        <circle r={eyeRadius} cx={eyeOffsetX} cy={-eyeOffsetY} />
-        <path d={mouthArc()} />
-      </g>
-    </svg>
-  );
-}
+const App = () => {
+  <Face
+    height={height}
+    width={width}
+    centerX={width / 2}
+    centerY={height / 2}
+    strokeWidth={20}
+    eyeOffsetX={90}
+    eyeOffsetY={100}
+    eyeRadius={40}
+    mouthWidth={20}
+    mouthRadius={140}
+  />;
+};
 
 export default App;
---
+++ src/components/BackgroundCircle.js
@@ -0,0 +1,4 @@
+export const BackgroundCircle = ({ radius,strokeWidth }) => (
+    <circle r={radius} fill="yellow" stroke="black" stroke-width={strokeWidth} />
+  );
+  
