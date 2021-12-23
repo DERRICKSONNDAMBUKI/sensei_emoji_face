4f00271838179c3d225f89282c61643a4e52e98b
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2021-12-23 19:36:31 +0300

    multiple emojis

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { Face } from "./components/Face";
+import {range} from 'd3'
+const width = 160;
+const height = 160;
 
-const width = 960;
-const height = 500;
+const array=range(5*5)
 
-const App = () => {
+const App = () => 
+ array.map(()=>(
   <Face
-    height={height}
-    width={width}
-    centerX={width / 2}
-    centerY={height / 2}
-    strokeWidth={20}
-    eyeOffsetX={90}
-    eyeOffsetY={100}
-    eyeRadius={40}
-    mouthWidth={20}
-    mouthRadius={140}
-  />;
-};
+  height={height}
+  width={width}
+  centerX={width / 2}
+  centerY={height / 2}
+  strokeWidth={6+Math.random()*3}
+  eyeOffsetX={20+Math.random()*9}
+  eyeOffsetY={20+Math.random()*15}
+  eyeRadius={5+Math.random()*10}
+  mouthWidth={7+Math.random()*9}
+  mouthRadius={30 +Math.random()*10}
+/>
+
+ ))
 
 export default App;
